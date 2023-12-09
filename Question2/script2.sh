08bd4a63f9bea38106c6669a4a00a22b9af028e4
Mohit Nair <mohitnair051204@gmail.com>
2023-12-09 20:44:43 +0530

    Correct Outputs

--- Question2/script2.sh
+++ Question2/script2.sh
@@ -10,8 +10,8 @@ fi
 
 # Output:
 # ❯ ls
-# a.txt  b.txt  README.md  script1.sh  script2.sh  student_db.csv  studentdb.sh  studentdb.txt
+# a.txt  b.txt   script2.sh
 # ❯ sh script2.sh a.txt b.txt
 # Files are identical. b.txt will be removed.
 # ❯ ls
-# a.txt  README.md  script1.sh  script2.sh  student_db.csv  studentdb.sh  studentdb.txt
+# a.txt  script2.sh
